95d4aebacc409ff4bf652b09ed07397cc185982d
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-24 18:16:40 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,210 @@
+const BG_COLOR = '#212529';
+const PLAYER_COLOR = 'green';
+const FOOD_COLOR = '#e66916';
+
+let canvas, ctx;
+
+const socket = io('http://localhost:8080');
+
+const gameSection = document.getElementById('gameSection');
+const gameScreen = document.getElementById('gameScreen');
+const initialScreen = document.getElementById('initialScreen');
+const newGameButton = document.getElementById('newGameButton');
+const joinGameButton = document.getElementById('joinGameButton');
+const usernameInput = document.getElementById('usernameInput');
+const gameCodeInput = document.getElementById('gameCodeInput');
+const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+
+const statsBoard = document.getElementById('statsBoard');
+const healthText = document.getElementById('health');
+const powerText = document.getElementById('power');
+const staminaText = document.getElementById('stamina');
+
+const startVoteScreen = document.getElementById('startVote');
+const userCount = document.getElementById('userCount');
+const voteCount = document.getElementById('voteCount');
+const voteButton = document.getElementById('voteButton');
+
+let roomName;
+let playerNumber;
+let playerCount;
+let currentStatus = 'Waiting...';
+let gameActive = false;
+
+const handleKeyDown = (e) => {
+  socket.emit('keydown', e.keyCode);
+};
+
+const init = () => {
+  initialScreen.style.display = 'none';
+  gameSection.style.display = 'flex';
+  gameScreen.style.display = 'inherit';
+
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+
+  canvas.width = canvas.height = 512;
+
+  ctx.fillStyle = 'brown';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = '2rem Arial';
+  ctx.fillStyle = 'white';
+  ctx.fillText(currentStatus, 40, canvas.height / 2);
+
+  document.addEventListener('keydown', handleKeyDown);
+  gameActive = false;
+};
+
+const paintGame = (state) => {
+  statsBoard.style.display = 'block';
+
+  ctx.fillStyle = BG_COLOR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const food = state.food;
+  const gridSize = state.gridSize;
+  const size = canvas.width / gridSize;
+
+  ctx.fillStyle = FOOD_COLOR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
+
+  const playerColors = [PLAYER_COLOR, 'red', 'yellow', 'blue'];
+
+  for (let i = 0; i < playerCount; i++) {
+    paintPlayer(state.players[i], size, playerColors[i]);
+  }
+};
+
+const paintPlayer = (playerState, size, color) => {
+  const player = playerState.pos;
+  const playerStats = playerState.stats;
+
+  if (playerState.id === playerNumber) {
+    healthText.innerText = playerStats.health;
+    powerText.innerText = playerStats.power;
+    staminaText.innerText = playerStats.stamina;
+  }
+
+  ctx.fillStyle = color;
+  ctx.fillRect(player.x * size, player.y * size, size, size);
+};
+
+const handleInit = (number) => {
+  playerNumber = number;
+};
+
+const handleGameState = (gameState) => {
+  if (!gameActive) {
+    return;
+  }
+
+  const serverGameState = JSON.parse(gameState);
+  requestAnimationFrame(() => paintGame(serverGameState));
+};
+
+const handleGameStatus = (gameStatus) => {
+  if (gameStatus === 'waiting') {
+    currentStatus = 'Waiting...';
+  } else if (gameStatus === 'started') {
+    gameActive = true;
+  } else {
+    if (gameStatus === 1) {
+      startVoteScreen.style.display = 'none';
+    }
+    currentStatus = `Starting in ${gameStatus}`;
+    init();
+  }
+};
+const handleGameCode = (gameCode) => {
+  gameCodeDisplay.innerText = gameCode;
+  roomName = gameCode;
+};
+
+const newGame = () => {
+  const username = usernameInput.value;
+  if (!username) {
+    alert('Please enter your username');
+  } else {
+    socket.emit('newGame', username);
+    init();
+  }
+};
+
+const joinGame = () => {
+  const code = gameCodeInput.value;
+  const username = usernameInput.value;
+  if (!code) {
+    alert('Please enter game code');
+  } else if (!username) {
+    alert('Please enter your username');
+  } else {
+    socket.emit('joinGame', code, username);
+    init();
+  }
+};
+
+const handleGameOver = (data) => {
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+
+  gameActive = false;
+
+  if (data.winner === playerNumber) {
+    alert('You Win!');
+  } else {
+    alert('You Lose :(');
+  }
+};
+
+const handleUnknownCode = () => {
+  reset();
+  alert('Unknown Game Code');
+};
+
+const handleTooManyPlayers = () => {
+  reset();
+  alert('This game is already in progress');
+};
+
+const handlePlayerCount = (numClients) => {
+  playerCount = numClients;
+  userCount.innerText = playerCount;
+};
+
+const handleVoteCount = (vote) => {
+  currentVoteCount = vote;
+
+  voteCount.innerText = `${currentVoteCount}/${playerCount}`;
+};
+
+const reset = () => {
+  playerNumber = null;
+  gameCodeInput.value = '';
+  gameSection.style.display = 'none';
+  initialScreen.style.display = 'block';
+};
+
+voteButton.addEventListener('click', () => {
+  if (playerCount >= 2) {
+    socket.emit('startVote', roomName);
+    voteButton.classList.add('voted');
+  } else {
+    alert(`You can't play alone :(`);
+  }
+});
+
+newGameButton.addEventListener('click', newGame);
+joinGameButton.addEventListener('click', joinGame);
+
+socket.on('init', handleInit);
+socket.on('gameState', handleGameState);
+socket.on('gameStatus', handleGameStatus);
+socket.on('gameOver', handleGameOver);
+socket.on('gameCode', handleGameCode);
+socket.on('unknownCode', handleUnknownCode);
+socket.on('tooManyPlayers', handleTooManyPlayers);
+socket.on('numClients', handlePlayerCount);
+socket.on('voteCount', handleVoteCount);
